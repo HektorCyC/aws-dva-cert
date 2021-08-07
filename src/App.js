0cf7097e06a9562a10c4b59248d75c5e53fff7d0
Hector Camarillo <hectorc@Hectors-MacBook-Pro.local>
2021-08-07 13:39:29 -0500

    Added backend to front end

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listTodos } from './graphql/queries';
+import { createTodo, deleteTodo } from './graphql/mutations';
+
 
 function App() {
+  const initialFormState = { name: "", description: "" };
 
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  // componentMount();
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const graphQL = await API.graphql({ query: listTodos }); // Fetch data from graphql
+    setNotes(graphQL.data.listTodos.items)
+  };
+
+  const createNote = async () => {
+    await API.graphql({ query: createTodo, variables: { input: formData } })
+    setNotes([...notes, formData]); //array.push(formData)
+    setFormData(initialFormState)
+  }
+
+  const deleteNote = async ({ id }) => {
+    await API.graphql({ query: { deleteTodo, variables: { input: id } } })
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setFormData(initialFormState)
+    setNotes(newNotesArray)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href={`ejemplo`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <AmplifySignOut/>
-      </header>
+      <h1>Mis notas</h1>
+      <input
+        onChange={event => setFormData({ ...formData, name: event.target.value })}
+        placeholder={"Nombre de la nota"}
+        value={formData.name}
+      />
+
+      <input
+        onChange={event => setFormData({ ...formData, description: event.target.value })}
+        placeholder={"Descripcion de la nota"}
+        value={formData.description}
+      />
+
+      <div style={{ marginBottom: 30 }}>
+        {notes.map(element => {
+          return (<div style={{ border: '1px solid black' }}>
+            <h2>{element.name}</h2>
+            <h3>{element.name}</h3>
+            <button onClick={deleteNote}>Borrar nota</button>
+          </div>)
+        })}
+      </div>
+
+      <button onClick={createNote}>Crear nota</button>
+      <AmplifySignOut />
     </div >
   );
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_Q9eVjbW8k",
     "aws_user_pools_web_client_id": "55946l32k0ade7f232mu2odbh5",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://xqaj5egv5rcipbc47jsvuygcue.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-yd4hzfpitvah5nihcvocpanp4q"
 };
 
 
